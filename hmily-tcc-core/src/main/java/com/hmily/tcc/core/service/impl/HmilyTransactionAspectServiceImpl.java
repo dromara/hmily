fa3383db4191947c9ec956e53670c49b67bc6f22
Administrator <549477611@qq.com>
2018-09-20 20:12:15 +0800

    sync commit and inline support.

--- hmily-tcc-core/src/main/java/com/hmily/tcc/core/service/impl/HmilyTransactionAspectServiceImpl.java
+++ hmily-tcc-core/src/main/java/com/hmily/tcc/core/service/impl/HmilyTransactionAspectServiceImpl.java
@@ -29,6 +29,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * HmilyTransactionAspectServiceImpl.
+ *
  * @author xiaoyu
  */
 @Service("tccTransactionAspectService")
@@ -43,12 +44,12 @@ public class HmilyTransactionAspectServiceImpl implements HmilyTransactionAspect
     }
 
     /**
-     * tcc 事务切面服务.
+     * hmily transaction aspect.
      *
-     * @param tccTransactionContext tcc事务上下文对象
-     * @param point                 切点
-     * @return object
-     * @throws Throwable 异常信息
+     * @param tccTransactionContext {@linkplain  TccTransactionContext}
+     * @param point                {@linkplain ProceedingJoinPoint}
+     * @return object  return value
+     * @throws Throwable  exception
      */
     @Override
     public Object invoke(final TccTransactionContext tccTransactionContext, final ProceedingJoinPoint point) throws Throwable {
