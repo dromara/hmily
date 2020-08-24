1e009addf5fd48704771f3f631de174d83f61d85
cysy-lli <746238836@qq.com>
2020-08-24 21:52:26 +0800

    hmily 配置  apollo实现 (#158)
    * hmily 配置  apollo实现
    
    * hmily-repository-all  添加zookeeper模块依赖
    
    * hmily-config-apollo 调整

---
+++ hmily-config/hmily-config-apollo/src/main/java/org/dromara/hmily/config/apollo/ApolloConfig.java
@@ -0,0 +1,35 @@
+package org.dromara.hmily.config.apollo;
+
+import lombok.Data;
+import org.dromara.hmily.config.api.AbstractConfig;
+import org.dromara.hmily.config.api.constant.PrefixConstants;
+import org.dromara.hmily.spi.HmilySPI;
+
+/**
+ * ApolloConfig.
+ * @author   lilang
+ **/
+@Data
+@HmilySPI("remoteApollo")
+public class ApolloConfig extends AbstractConfig {
+
+    private String appId;
+
+    private String namespace;
+
+    private String configService;
+
+    private String fileExtension;
+
+    private String secret;
+
+    private String meta;
+
+    private String env;
+
+    @Override
+    public String prefix() {
+        return PrefixConstants.REMOTE_APOLLO;
+    }
+
+}
