99ea4bcab09b3f100fc85ccc8a0c56644fefc453
yu199195 <549477611@qq.com>
2020-08-25 20:34:18 +0800

    checkstyle

--- hmily-core/src/main/java/org/dromara/hmily/core/disruptor/publisher/HmilyRepositoryEventPublisher.java
+++ hmily-core/src/main/java/org/dromara/hmily/core/disruptor/publisher/HmilyRepositoryEventPublisher.java
@@ -86,20 +86,6 @@ public final class HmilyRepositoryEventPublisher implements AutoCloseable {
     }
     
     /**
-     * Async publish event.
-     *
-     * @param hmilyTransaction the hmily transaction
-     * @param type             the type
-     */
-    public void asyncPublishEvent(final HmilyTransaction hmilyTransaction, final int type) {
-        HmilyRepositoryEvent event = new HmilyRepositoryEvent();
-        event.setType(type);
-        event.setHmilyTransaction(hmilyTransaction);
-        event.setTransId(hmilyTransaction.getTransId());
-        disruptorProviderManage.getProvider().onData(event);
-    }
-    
-    /**
      * Publish event.
      *
      * @param hmilyParticipantUndo the hmily participant undo
@@ -125,6 +111,20 @@ public final class HmilyRepositoryEventPublisher implements AutoCloseable {
         event.setTransId(hmilyParticipant.getTransId());
         event.setHmilyParticipant(hmilyParticipant);
         push(event);
+    }
+    
+    /**
+     * Async publish event.
+     *
+     * @param hmilyTransaction the hmily transaction
+     * @param type             the type
+     */
+    public void asyncPublishEvent(final HmilyTransaction hmilyTransaction, final int type) {
+        HmilyRepositoryEvent event = new HmilyRepositoryEvent();
+        event.setType(type);
+        event.setHmilyTransaction(hmilyTransaction);
+        event.setTransId(hmilyTransaction.getTransId());
+        disruptorProviderManage.getProvider().onData(event);
     }
     
     private void push(final HmilyRepositoryEvent event) {
