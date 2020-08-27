7841a7c7ee2464b527055a33c0f6fde5a84b51ed
baige <bbaiggey@163.com>
2020-08-27 17:56:10 +0800

    add motan demo

---
+++ hmily-demo/hmily-demo-motan/hmily-demo-motan-account-api/src/main/java/org/dromara/hmily/demo/motan/account/api/service/InlineService.java
@@ -0,0 +1,12 @@
+package org.dromara.hmily.demo.motan.account.api.service;
+
+/**
+ * @author bbaiggey(Myth)
+ */
+public interface InlineService {
+
+    /**
+     * 测试内嵌的时候处理.
+     */
+    void testInline();
+}
