16b816cba0bb1f03923d1a65d2a20eaf1db9701e
cysy-lli <746238836@qq.com>
2020-09-22 23:55:29 +0800

    etcd 自动更新配置

--- hmily-config/hmily-config-etcd/src/main/java/org/dromara/hmily/config/etcd/EtcdClient.java
+++ hmily-config/hmily-config-etcd/src/main/java/org/dromara/hmily/config/etcd/EtcdClient.java
@@ -4,9 +4,14 @@ import com.coreos.jetcd.Client;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.data.KeyValue;
 import com.coreos.jetcd.kv.GetResponse;
+import com.coreos.jetcd.watch.WatchEvent;
 import org.dromara.hmily.common.utils.CollectionUtils;
 import org.dromara.hmily.common.utils.StringUtils;
+import org.dromara.hmily.config.api.event.AddData;
+import org.dromara.hmily.config.api.event.EventData;
+import org.dromara.hmily.config.api.event.RemoveData;
 import org.dromara.hmily.config.api.exception.ConfigException;
+import org.dromara.hmily.config.loader.ConfigLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +22,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
 /**
  * etcd client.
@@ -26,6 +32,8 @@ import java.util.concurrent.TimeoutException;
 public class EtcdClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EtcdClient.class);
+
+    private Client client;
 
     /**
      * Pull input stream.
@@ -57,5 +65,44 @@ public class EtcdClient {
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new ConfigException(e);
         }
+    }
+
+    /**
+     * Add listener.
+     *
+     * @param context        the context
+     * @param passiveHandler the passive handler
+     * @param config         the config
+     * @throws InterruptedException exception
+     */
+    void addListener(final Supplier<ConfigLoader.Context> context, final ConfigLoader.PassiveHandler<EtcdPassiveConfig> passiveHandler, final EtcdConfig config) throws InterruptedException {
+        if (!config.isPassive()) {
+            return;
+        }
+        if (client == null) {
+            LOGGER.warn("Etcd client is null...");
+        }
+        client.getWatchClient().watch(ByteSequence.fromString(config.getKey())).listen().getEvents().stream().forEach(watchEvent -> {
+            WatchEvent.EventType eventType = watchEvent.getEventType();
+            KeyValue keyValue = watchEvent.getKeyValue();
+            KeyValue prevKV = watchEvent.getPrevKV();
+            EventData eventData = null;
+            switch (eventType) {
+                case PUT:
+                    eventData = new AddData(keyValue.getKey().toString(), keyValue.getValue().toStringUtf8());
+                    break;
+                case DELETE:
+                    eventData = new RemoveData(prevKV.getKey().toStringUtf8(), null);
+                    break;
+                default:
+                    break;
+            }
+            EtcdPassiveConfig etcdPassiveConfig = new EtcdPassiveConfig();
+            etcdPassiveConfig.setKey(config.getKey());
+            etcdPassiveConfig.setFileExtension(config.getFileExtension());
+            etcdPassiveConfig.setValue(eventData);
+            passiveHandler.passive(context, etcdPassiveConfig);
+        });
+        LOGGER.info("passive Etcd remote started....");
     }
 }
