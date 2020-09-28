576a58a242e479213f18fa884c5b81c775452e32
tydhot <583125614@qq.com>
2020-09-28 10:01:45 +0800

    add hmily tars demo

--- hmily-demo/hmily-demo-tars/hmily-demo-tars-springboot-order/src/main/java/org/dromara/hmily/demo/tars/order/service/impl/PaymentServiceImpl.java
+++ hmily-demo/hmily-demo-tars/hmily-demo-tars-springboot-order/src/main/java/org/dromara/hmily/demo/tars/order/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,7 @@
 package org.dromara.hmily.demo.tars.order.service.impl;
 
 import com.qq.tars.spring.annotation.TarsClient;
+import org.dromara.hmily.annotation.HmilyTCC;
 import org.dromara.hmily.demo.common.order.entity.Order;
 import org.dromara.hmily.demo.common.order.enums.OrderStatusEnum;
 import org.dromara.hmily.demo.common.order.mapper.OrderMapper;
@@ -9,6 +10,8 @@ import org.dromara.hmily.demo.tars.order.servant.inventoryapp.InventoryPrx;
 import org.dromara.hmily.demo.tars.order.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 /**
  * @Author tydhot
@@ -30,6 +33,7 @@ public class PaymentServiceImpl implements PaymentService{
     }
 
     @Override
+    @HmilyTCC(confirmMethod = "confirmOrderStatus", cancelMethod = "cancelOrderStatus")
     public void makePayment(Order order) {
         updateOrderStatus(order, OrderStatusEnum.PAYING);
         accountPrx.payment(order.getUserId(), order.getTotalAmount().doubleValue());
@@ -39,5 +43,15 @@ public class PaymentServiceImpl implements PaymentService{
     private void updateOrderStatus(Order order, OrderStatusEnum orderStatus) {
         order.setStatus(orderStatus.getCode());
         orderMapper.update(order);
+    }
+
+    public void confirmOrderStatus(Order order) {
+        updateOrderStatus(order, OrderStatusEnum.PAY_SUCCESS);
+        LOGGER.info("=========进行订单confirm操作完成================");
+    }
+
+    public void cancelOrderStatus(Order order) {
+        updateOrderStatus(order, OrderStatusEnum.PAY_FAIL);
+        LOGGER.info("=========进行订单cancel操作完成================");
     }
 }
