c7b9fc4e305a37eebc3fe2d362ccb96ae4cb38c5
cherrylzhao <zhaojunv3@126.com>
2020-10-07 00:18:04 +0800

    implement generate query image SQLs for update compute engine

--- hmily-tac/hmily-tac-sqlcompute/src/main/java/org/dromara/hmily/tac/sqlcompute/impl/HmilyUpdateSQLComputeEngine.java
+++ hmily-tac/hmily-tac-sqlcompute/src/main/java/org/dromara/hmily/tac/sqlcompute/impl/HmilyUpdateSQLComputeEngine.java
@@ -17,16 +17,24 @@
 
 package org.dromara.hmily.tac.sqlcompute.impl;
 
+import com.google.common.base.Joiner;
 import lombok.RequiredArgsConstructor;
 import org.dromara.hmily.repository.spi.entity.HmilySQLTuple;
 import org.dromara.hmily.repository.spi.entity.HmilyUndoInvocation;
 import org.dromara.hmily.tac.sqlcompute.exception.SQLComputeException;
+import org.dromara.hmily.tac.sqlparser.model.segment.dml.expr.complex.HmilyCommonExpressionSegment;
+import org.dromara.hmily.tac.sqlparser.model.segment.dml.expr.simple.HmilyLiteralExpressionSegment;
+import org.dromara.hmily.tac.sqlparser.model.segment.dml.item.HmilyExpressionProjectionSegment;
+import org.dromara.hmily.tac.sqlparser.model.segment.generic.table.HmilySimpleTableSegment;
 import org.dromara.hmily.tac.sqlparser.model.statement.dml.HmilyUpdateStatement;
 
 import java.sql.Connection;
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Hmily update SQL compute engine.
@@ -61,8 +69,54 @@ public final class HmilyUpdateSQLComputeEngine extends AbstractHmilySQLComputeEn
     }
     
     @Override
-    List<ImageSQLUnit> generateQueryImageSQLs(final String originalSQL) {
+    Collection<ImageSQLUnit> generateQueryImageSQLs(final String sql) {
         // TODO generate image SQL group according to parsed statement
-        return null;
+        Collection<ImageSQLUnit> result = new LinkedList<>();
+        String tables = getTables(sql);
+        String whereCondition = getWhereCondition(sql);
+        statement.getTables().forEach(segment -> {
+            String imageSQL = String.format("SELECT %s FROM %s %s", Joiner.on(",").join(getUndoItems(segment), getRedoItems(segment)), tables, whereCondition);
+            result.add(new ImageSQLUnit(imageSQL, new LinkedList<>(), "update", sql.substring(segment.getStartIndex(), segment.getStopIndex())));
+        });
+        return result;
+    }
+    
+    private String getUndoItems(final HmilySimpleTableSegment segment) {
+        String result;
+        if (segment.getAlias().isPresent()) {
+            result = String.format("%s.*", segment.getAlias().get());
+        } else if (segment.getOwner().isPresent()) {
+            result = String.format("%s.%s.*", segment.getOwner(), segment.getTableName().getIdentifier().getValue());
+        } else {
+            result = String.format("%s.*", segment.getTableName().getIdentifier().getValue());
+        }
+        return result;
+    }
+    
+    private List<String> getRedoItems(final HmilySimpleTableSegment tableSegment) {
+        List<String> result = new LinkedList<>();
+        // TODO filter the column which don't belong to current table
+        statement.getSetAssignment().getAssignments().forEach(assignment -> {
+            Object value;
+            if (assignment.getValue() instanceof HmilyLiteralExpressionSegment) {
+                value = ((HmilyLiteralExpressionSegment) assignment.getValue()).getLiterals();
+            } else if (assignment.getValue() instanceof HmilyExpressionProjectionSegment) {
+                value = ((HmilyExpressionProjectionSegment) assignment.getValue()).getText();
+            } else {
+                value = ((HmilyCommonExpressionSegment) assignment.getValue()).getText();
+            }
+            result.add(String.format("%s AS $after_image$%s", value, assignment.getColumn().getIdentifier().getValue()));
+        });
+        return result;
+    }
+    
+    private String getTables(final String sql) {
+        List<String> tables = statement.getTables().stream().map(segment -> sql.substring(segment.getStartIndex(), segment.getStopIndex())).collect(Collectors.toList());
+        return Joiner.on(",").join(tables);
+    }
+    
+    private String getWhereCondition(final String sql) {
+        return statement.getWhere().map(segment -> sql.substring(segment.getStartIndex(), segment.getStopIndex()))
+            .orElseThrow(() -> new SQLComputeException("DML SQL should contain where condition"));
     }
 }
