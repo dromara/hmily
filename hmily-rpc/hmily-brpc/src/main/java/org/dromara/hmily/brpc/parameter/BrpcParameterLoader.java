e586c531098a996fdac65a88a2f8041800e95d00
liuyueve <39432700+liuyueve@users.noreply.github.com>
2020-10-12 10:50:27 +0800

    添加 apache lincese ，修改注释

--- hmily-rpc/hmily-brpc/src/main/java/org/dromara/hmily/brpc/parameter/BrpcParameterLoader.java
+++ hmily-rpc/hmily-brpc/src/main/java/org/dromara/hmily/brpc/parameter/BrpcParameterLoader.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dromara.hmily.brpc.parameter;
 
 import com.baidu.brpc.RpcContext;
@@ -11,9 +28,9 @@ import java.util.Map;
 import java.util.Optional;
 
 /**
- * Create by liu·yu
- * Date is 2020-10-08
- * Description is :
+ * The hmily brpc parameter loader
+ *
+ * @author liu·yu
  */
 @HmilySPI(value = "brpc")
 public class BrpcParameterLoader implements RpcParameterLoader {
