ad6e49c510e20d7030e6c3262e9ba2791482d549
Zhang Yonglun <zhangyonglun@apache.org>
2021-05-21 15:28:35 +0800

    add oracle, postgresql, sqlserver dialect

---
+++ hmily-tac/hmily-tac-sqlparser/hmily-tac-sqlparser-model/src/main/java/org/dromara/hmily/tac/sqlparser/model/dialect/sqlserver/dml/HmilySQLServerDeleteStatement.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dromara.hmily.tac.sqlparser.model.dialect.sqlserver.dml;
+
+import lombok.Setter;
+import lombok.ToString;
+import org.dromara.hmily.tac.sqlparser.model.common.segment.generic.HmilyOutputSegment;
+import org.dromara.hmily.tac.sqlparser.model.common.segment.generic.HmilyWithSegment;
+import org.dromara.hmily.tac.sqlparser.model.common.statement.dml.HmilyDeleteStatement;
+import org.dromara.hmily.tac.sqlparser.model.dialect.sqlserver.HmilySQLServerStatement;
+
+import java.util.Optional;
+
+/**
+ * SQLServer delete statement.
+ */
+@Setter
+@ToString
+public final class HmilySQLServerDeleteStatement extends HmilyDeleteStatement implements HmilySQLServerStatement {
+
+    private HmilyWithSegment withSegment;
+    
+    private HmilyOutputSegment outputSegment;
+
+    /**
+     * Get with segment.
+     *
+     * @return with segment.
+     */
+    public Optional<HmilyWithSegment> getWithSegment() {
+        return Optional.ofNullable(withSegment);
+    }
+    
+    /**
+     * Get output segment.
+     *
+     * @return output segment.
+     */
+    public Optional<HmilyOutputSegment> getOutputSegment() {
+        return Optional.ofNullable(outputSegment);
+    }
+}
